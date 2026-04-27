5a4dbd2f6d5d2b19687f937439df621fef89d047
Oscar Paul Janossy <57064478+0scarjny@users.noreply.github.com>
2026-04-27 22:59:39 +0200

    added m5stack development files

---
+++ weather-monitor/m5stack/api_func.py
@@ -0,0 +1,36 @@
+# data_sender.py
+# API functions
+
+import json
+import requests
+
+DEBUG = True  # flip to False once everything works
+IPDATA_KEY = "e2f1b4d9820c7256c8ccf858c57a98d71319a84664c01d23886f1ef6"
+
+
+def _log(*args):
+    if DEBUG:
+        print("[api]", *args)
+        
+#################################
+def get_location_from_ip(api_key: str):
+    """
+    Fetches the location based on IP using the ipdata.co API.
+    Returns the city if found, otherwise returns the region.
+    """
+    url = f"https://api.ipdata.co/?api-key={api_key}"
+    headers = {"accept": "application/json"}
+    
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()  # Raises an exception for 4xx or 5xx errors
+        
+        data = response.json()
+        
+        # Return city if it exists and is not empty, otherwise return region
+        return data.get("city") or data.get("region")
+        
+    except requests.RequestException as e:
+        _log(f"Error fetching location data: {e}")
+        return "Lausanne" # Default if no connection
+
